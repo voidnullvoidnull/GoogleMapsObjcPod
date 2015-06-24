7a80e9238adaee20c4fbd28dbe2f2d2fd556eccd
ziyang <ziyang0621@gmail.com>
2015-06-24 11:04:35 -0700

    Initial Commit

---
+++ GoogleMapsObjCPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleMapsObjCPod
+//
+//  Created by Ziyang Tan on 6/24/15.
+//  Copyright (c) 2015 Ziyang Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoogleMapsObjCPod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleMapsObjCPod
+//
+//  Created by Ziyang Tan on 6/24/15.
+//  Copyright (c) 2015 Ziyang Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
